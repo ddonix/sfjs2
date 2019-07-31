716a0e2b3a4bcc52d18b6167bb5ad99d87e593e7
ddonix <18959661712@189.cn>
2019-07-31 11:51:48 +0800

    1593

---
+++ 1593.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+const int MAXLINE = 1000+5;
+const int MAXCOL = 180;
+
+vector<string> data[MAXLINE];
+
+int main()
+{
+	int i;
+	string s;
+
+	int cn;
+	int cnm[MAXCOL];
+
+	i = 0;
+	while(cin>>s)
+	{
+		istringstream ss(s);
+	}
+	return 0;
+}
--- makefile
+++
@@ -1,4 +0,0 @@
-all:1_1.exe 
-
-1_1.exe:1_1.cpp
-	g++ 1_1.cpp -o 1_1.exe
