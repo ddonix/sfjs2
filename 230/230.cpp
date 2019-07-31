072071df056d831b2cdee360087dadd56d5745ca
ddonix <18959661712@189.cn>
2019-08-01 07:18:03 +0800

    d

---
+++ 230/230.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	vector<string> v;
+	string s;
+	while(getline(cin,s) && s != "END")
+	{
+		cout<<s<<endl;
+
+	}
+}
