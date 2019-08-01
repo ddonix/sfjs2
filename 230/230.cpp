09681822a554d29b174a5a92ddaa785267f358ec
ddonix <18959661712@189.cn>
2019-08-01 09:41:56 +0800

    das

--- 230/230.cpp
+++ 230/230.cpp
@@ -2,16 +2,74 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <stack>
+#include <set>
 
 using namespace std;
 
+struct Book
+{
+	string title;
+	string author;
+
+	Book(string s)
+	{
+		string::size_type p = s.find("\"", 1);
+		title = s.substr(1, p-1);
+		author = s.substr(p+5);
+	}
+
+	//其实是<=
+	bool operator == (const Book & right) const
+	{
+		if (author < right.author)
+			return true;
+		else if(author > right.author)
+			return false;
+		else if(title <= right.title)
+			return true;
+		return false;
+	}
+};
+
+int cmp(const Book & a, const Book & b)
+{
+	if (a.author < b.author)
+		return 1;
+	else if(a.author > b.author)
+		return 0;
+	return a.title < b.title;
+}
+
 int main()
 {
-	vector<string> v;
+	vector<Book> v;
 	string s;
-	while(getline(cin,s) && s != "END")
+	while(getline(cin,s) && s != "END" && s != "END\r")
 	{
-		cout<<s<<endl;
-
+		v.push_back(Book(s));
 	}
+	sort(v.begin(), v.end(), cmp);
+
+	find(v.begin(), v.end(), Book("asd"));
+
+	stack<string> rt;
+	while(getline(cin,s) && s != "END" && s != "END\r")
+	{
+		string s1;
+		cout<<s<<endl;
+		if (s[0] == 'B')
+		{
+			s1 = s.substr(8, s.length()-2);
+		}
+		else if (s[0] == 'R')
+		{
+			s1 = s.substr(8, s.length()-2);
+		}
+		else
+		{
+		
+		}
+	}
+	cout<<"END"<<endl;
 }
