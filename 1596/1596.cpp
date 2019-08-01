4e64bbb9b0455897942ffbf84e82629468dc86aa
ddonix <18959661712@189.cn>
2019-08-01 17:58:35 +0800

    dd

---
+++ 1596/1596.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+#include <set>
+
+bool addr(const string & p)
+{
+
+}
+
+//-1:not init
+//-2:not be value
+//0-(2**31-1): normal
+int value(const string & p)
+{
+
+}
+
+int main()
+{
+	
+}
