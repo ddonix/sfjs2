4fcf79ad0ae078b86dbf9405d28985502b0fc416
ddonix <18959661712@189.cn>
2019-08-06 14:09:22 +0800

    21

--- 212/212.cpp
+++ 212/212.cpp
@@ -3,6 +3,7 @@
 #include <queue>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,26 +18,39 @@ struct Patient
 	int state; 	//0:wo;1:oing; 2:ting; 3:ring;4:ok
 
 	int orid;
-	int sbt;
-	int set;
+	int obt;
+	int oet;
+	
 	int rrid;
 	int rbt;
 	int ret;
 	
-	Patient() :state(0) {}
-	Patient(string n, int st, int rt) : name(n), surgeryt(st), recovert(rt){}
+	Patient() : state(0) {}
+	Patient(string n, int st, int rt) : name(n), state(0), surgeryt(st), recovert(rt){}
 };
 
 struct Room
 {
+	int id;		//
+	
 	int state;	//0: avaliable, 1:using, 2:prepare
+	int p;		//patient
 	int bt;		//begin time, -1: avaliable
 	int et;		//end tiem
-	int p;		//patient
+	
 	Room() : state(0), usingt(0) {}
 	int usingt;
 };
 
+struct Find_by_state
+{
+	Find_by_state(int s) : state(s) {}
+	bool operator()(const Room &r) 
+	{
+		return state == r.state;
+	}
+	int state;
+};
 vector<Patient> patient;
 vector<Room> oroom;
 vector<Room> rroom;
@@ -56,61 +70,112 @@ int main()
 		cin>>name>>st>>rt;
 		patient[i] = Patient(name, st, rt);
 	}
+	for(int i = 0; i < no; i++)
+		oroom[i].id = i;
+	for(int i = 0; i < nr; i++)
+		rroom[i].id = i;
+	
 	printf(" Patient          Operating Room          Recovery Room\n");
 	printf(" #  Name     Room#  Begin   End      Bed#  Begin    End\n");
 	printf(" ------------------------------------------------------\n");
 	
 	c_time = 0;
-	while(1)
+	int okp = 0;
+	while(okp < np)
 	{
-		int oa = -1, ra = -1;
 		int newt = INF;
 
 		for(int i = 0; i < nr; i++)
 		{
-			if (rroom[i].state == 0)
-			{
-				if (ra < 0)
-					ra = i;
-				continue;
-			}
-			if(rroom[i].et != c_time)
-				continue;
-			if(rroom[i].state == 1)
+			if((1 == rroom[i].state) && (rroom[i].et == c_time))
 			{
 				rroom[i].state = 2;
 				rroom[i].bt = c_time;
 				rroom[i].et = c_time+rpt;
 				newt = (newt < rroom[i].et) ? newt : rroom[i].et;
+
+				patient[rroom[i].p].state++;
+				okp++;
+			}
+			else if((2 == rroom[i].state) && (rroom[i].et == c_time))
+			{
+				rroom[i].state = 0;
 			}
 		}
 		for(int i = 0; i < no; i++)
 		{
-			if (oroom[i].state == 0)
-			{
-				if (oa < 0)
-					oa = i;
-				continue;
-			}
-			if(oroom[i].et != c_time)
-				continue;
-			if(oroom[i].state = 1)
+			if((1 == oroom[i].state) && (oroom[i].et == c_time))
 			{
 				oroom[i].state = 2;
 				oroom[i].bt = c_time;
 				oroom[i].et = c_time+opt;
 				newt = (newt < oroom[i].et) ? newt : oroom[i].et;
+				
+				Patient & p = patient[oroom[i].p];
+				p.state++;
+				newt = (newt < p.oet+transt) ? newt : (p.oet+transt);
 			}
-			else
+			else if((2 == oroom[i].state) && (oroom[i].et == c_time))
 			{
 				oroom[i].state = 0;
 			}
 		}
-		break;
+
+		if(okp == np)
+			break;
+		
+		for(int i = 0; i < np; i++)
+		{
+			if(patient[i].state == 0)
+			{
+				auto it = find_if(oroom.begin(), oroom.end(), Find_by_state(0));
+				if (it != oroom.end())
+				{
+					Patient & p = patient[i];
+					Room & r = *it;
+					
+					p.state = 1;
+					p.orid = r.id;
+					p.obt = c_time;
+					p.oet = c_time+p.surgeryt;
+					newt = p.oet;
+
+					r.state = 1;
+					r.p = i;
+					r.bt = c_time;
+					r.et = c_time+p.surgeryt;
+					r.usingt += p.surgeryt;
+				}
+			}
+			else if(patient[i].state == 2 && (c_time >= (patient[i].oet+transt)))//must c_time == ()
+			{
+				auto it = find_if(rroom.begin(), rroom.end(), Find_by_state(0));//it != rroom.end()
+				Patient & p = patient[i];
+				Room & r = *it;
+					
+				p.state = 3;
+				p.rrid = r.id;
+				p.rbt = c_time;
+				p.ret = c_time+p.surgeryt;
+				newt = (newt < p.ret) ? newt : p.ret;
+
+				r.state = 1;
+				r.p = i;
+				r.bt = c_time;
+				r.et = c_time+p.surgeryt;
+				r.usingt += p.recovert;
+			}
+		}
+		c_time++;// = newt;
 	}
 	cout<<"Facility Utilization"<<endl;
 	cout<<"Type  # Minutes  \% Used"<<endl;
 	cout<<"-------------------------"<<endl;
+	for(int i = 0; i < np; i++)
+	{
+		Patient & p = patient[i];
+		printf("%d %s%d\n", i+1, p.name, p.orid);
+	}
 	for(int i = 0; i < no; i++)
 	{
 		printf("Room %2d%8d%8.2llf\n", i, oroom[i].usingt, (double)oroom[i].usingt/endtime);
