5f5c6edc5429353a2d9c70f94a786cfba5ce4fa7
ddonix <18959661712@189.cn>
2019-08-08 17:54:40 +0800

    442

--- 442/442.cpp
+++ 442/442.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <map>
 #include <stack>
+#include <string>
 
 using namespace std;
 
@@ -9,24 +10,65 @@ struct Mat
 	int x;
 	int y;
 
-	Mat(){}
+	int c;
+
+	Mat():x(-1), y(0), c(-1) {}
 	Mat(int xx, int yy) : x(xx), y(yy) {}
-	int mul(const Mat & a, const Mat & b)
+	bool mul(const Mat & a, const Mat & b)
 	{
 		if(a.y != b.x)
-			return -1;
+		{
+			c = -1;
+			return false;
+		}
 		else
 		{
 			x = a.x;
 			y = b.y;
-			return a.x * a.y * b.y;
+			c = a.x * a.y * b.y + a.c + b.c;
+			return true;
 		}
 	}
 };
 
+map<char, Mat> mat;
+
+void sum(string e, Mat & res)
+{
+	if (e.length() == 1)
+	{
+		if (mat.count(e[0]))
+		{
+			res.x = mat[e[0]].x; 
+			res.y = mat[e[0]].y; 
+			res.c = 0;
+		}
+	}
+	else if (e.length() == 2)
+	{
+		if (mat.count(e[0]) && mat.count(e[1]))
+			res.mul(mat[e[0]], mat[e[1]]);
+	}
+	else
+	{
+		Mat res1, res2;
+		string s;
+		string s1, s2;
+		s =  e.substr(1, s.length()-2);
+		if(s[0] != '(')
+		{
+			s1 = e.substr(0,1);
+			s2 = e.substr(1, s.length()-1);
+		}
+		else
+		{
+		
+		}
+	}
+}
+
 int main()
 {
-	map<char, Mat> mat;
 	int nm;
 	cin>>nm;
 	for(int i = 0; i < nm; i++)
@@ -35,6 +77,16 @@ int main()
 		int x, y;
 		cin>>a>>x>>y;
 		mat[a] = Mat(x,y);
+	}
+	string e;
+	while(cin>>e && e != "")
+	{
+		Mat res;
+		sum(e, res);
+		if (res.c < 0)
+			cout<<"error"<<endl;
+		else
+			cout<<res.c<<endl;
 	}
 
 	return 0;
