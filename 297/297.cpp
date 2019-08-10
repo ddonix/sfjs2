31ba367c1c5b26ae1aab2781dfebd67a8059b29f
ddonix <18959661712@189.cn>
2019-08-10 09:40:46 +0800

    297

--- 297/297.cpp
+++ 297/297.cpp
@@ -6,22 +6,22 @@ using namespace std;
 
 struct Node
 {
-	int value;
+	int t;		//type of node: 0:p(grey) ,1:empty(white), 2:full(black), -1:null
+	int b;		//number of black bolock
+	int d;		//root d: 256
 	
 	int c[4];
-	Node(int v) : value(v) {}
 	Node() {}
 };
 
-const int maxn = 20000;
+const int maxn = 200000;
 Node nodepool[maxn];
 
 int nn = 0;
 int newNode()
 {
 	nn++;
-	nodepool[nn].value=0;
-	for(int i = 0; i < 4; i++) nodepool[nn].c[i] = 0;
+	nodepool[nn].t = -1;
 	return nn;
 }
 
@@ -32,47 +32,49 @@ int getquadtree(int r, stringstream & ss)
 	ss>>color;
 	if ('p' == color)
 	{
-		nodepool[r].value = 0;
+		nodepool[r].t = 0;
+		nodepool[r].b = 0;
 		for(int i = 0; i < 4; i++)
 		{
 			ch = newNode();
+			nodepool[ch].d = nodepool[r].d >> 2;
 			getquadtree(ch, ss);
-			nodepool[r].c[0] = ch;
+			nodepool[r].b += nodepool[ch].b;
+			nodepool[r].c[i] = ch;
 		}
 	}
 	else if('e' == color)
-		nodepool[r].value = 1;
-	else
-		nodepool[r].value = 2;
-	return 0;
-}
-
-int black(int r)
-{
-	if (nodepool[r].value)
-		return nodepool[r].value-1;
+	{
+		nodepool[r].t = 1;
+		nodepool[r].b = 0;
+	}
 	else
 	{
-		int b = 0;
-		for(int i = 0; i < 4; i++) b += black(nodepool[r].c[i]);
-		return b;
+		nodepool[r].t = 2;
+		nodepool[r].b = nodepool[r].d;
 	}
+	return 0;
 }
 
 int mergeblack(int qrt1, int qrt2)
 {
-	if (0 == nodepool[qrt1].value)
-		return black(qrt2);
-	else if (1 == nodepool[qrt1].value)
-		return 1;
+	if (1 == nodepool[qrt1].t)
+		return nodepool[qrt2].b;
+	else if (2 == nodepool[qrt1].t)
+		return nodepool[qrt1].b;
 	else
 	{
-		if (0 == nodepool[qrt1].value)
-			return black(qrt2);
-		else if (1 == nodepool[qrt1].value)
-			return 1;
+		if (1 == nodepool[qrt2].t)
+			return nodepool[qrt1].b;
+		else if (2 == nodepool[qrt2].t)
+			return nodepool[qrt2].b;
+		else
+		{
+			int b = 0;
+			for(int i = 0; i < 4; i++) b += mergeblack(nodepool[qrt1].c[i], nodepool[qrt2].c[i]);
+			return b;
+		}
 	}
-	return 0;
 }
 
 int main()
@@ -82,7 +84,7 @@ int main()
 	while(T--)
 	{
 		int bpix;
-		char s[1000];
+		string s;
 		int qroot1, qroot2;
 		
 		nn = 0;
@@ -90,11 +92,13 @@ int main()
 		cin>>s;
 		stringstream ss1(s);
 		qroot1 = newNode();
+		nodepool[qroot1].d = 1024;
 		getquadtree(qroot1, ss1);
 		
 		cin>>s;
 		stringstream ss2(s);
 		qroot2 = newNode();
+		nodepool[qroot2].d = 1024;
 		getquadtree(qroot2, ss2);
 		
 		bpix = mergeblack(qroot1, qroot2);
