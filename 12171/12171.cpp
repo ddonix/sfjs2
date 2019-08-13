0eea7ecfe2934d943809202705f9b2af94d03cc9
ddonix <18959661712@189.cn>
2019-08-13 17:49:14 +0800

    d

--- 12171/12171.cpp
+++ 12171/12171.cpp
@@ -58,15 +58,21 @@ struct Index
 	{
 		Index temp = Index(id[0], id[1], id[2]);
 		if (fx == 0) temp.id[0]++;
-		if (fx == 1) temp.id[1]++;
-		if (fx == 2) temp.id[2]++;
-		if (fx == 10) temp.id[0]--;
-		if (fx == 11) temp.id[1]--;
-		if (fx == 12) temp.id[2]--;
+		if (fx == 1) temp.id[0]--;
+		if (fx == 2) temp.id[1]++;
+		if (fx == 3) temp.id[1]--;
+		if (fx == 4) temp.id[2]++;
+		if (fx == 5) temp.id[2]--;
 		return temp;
 	}
 	
-	bool operator< (const Index & right) const { return true; }
+	bool operator< (const Index & right) const 
+	{ 
+		for(int i = 0; i < 3; i++)
+			if (id[i] != right.id[i])
+				return id[i] < right.id[i];
+		return false;
+	}
 };
 
 //Box box[maxn];		//0 --- n-1铜盒子, n -- n+nc-1 空气盒子
@@ -76,7 +82,7 @@ struct Box
 	Adis a1;
 	int t;		//0:铜 1:空气
 	int touch;	//面积
-	int v;
+	int volume;
 	int edge[6];
 
 	Box(){}
@@ -85,13 +91,38 @@ struct Box
 		a0 = l;
 		a1 = r;
 		t = 0;
-		v =  (a1.a[0]-a0.a[0])*(a1.a[1]-a0.a[1])*(a1.a[2]-a0.a[2]);
+		volume =  (a1.a[0]-a0.a[0])*(a1.a[1]-a0.a[1])*(a1.a[2]-a0.a[2]);
 	}
 
-	bool isin(string type, int u0, int v0, int u1, int v1)
+	int relation(const Box & b)
 	{
-		pair<int, int> p0, p1;
-		return 0;
+		int u0, v0, u1, v1;
+		int o0, p0, o1, p1;
+		for(int i = 0; i < 3;i++)
+		{
+			u0 = ((i==0) ? a0.a[1] : a0.a[0]);
+			u1 = ((i==0) ? a1.a[1] : a1.a[0]);
+			v0 = ((i==2) ? a0.a[2] : a0.a[1]);
+			v1 = ((i==2) ? a1.a[2] : a1.a[1]);
+			
+			o0 = ((i==0) ? b.a0.a[1] : b.a0.a[0]);
+			o1 = ((i==0) ? b.a1.a[1] : b.a1.a[0]);
+			p0 = ((i==2) ? b.a0.a[2] : b.a0.a[1]);
+			p1 = ((i==2) ? b.a1.a[2] : b.a1.a[1]);
+			if((u0 <= o0) && (u1 >= o1) && (v0 <= p0) &&(v1 >= p1))
+			{
+				if (a0.a[i] > b.a1.a[i] || a1.a[i] < b.a0.a[i])
+					return -1;		//分离
+				if (a0.a[i] == b.a0.a[i] || a1.a[i] == b.a1.a[i])
+					return -2;		//重合
+				if (a0.a[i] == b.a1.a[i])
+					return 2*i;
+			 	else if	(a1.a[i] == b.a0.a[i])
+					return 2*i+1;
+
+			}
+		}
+		return -1;
 	}
 };
 
@@ -105,18 +136,23 @@ int nc;
 int area;
 int volume;
 
-void makeBox(int i)
+Box makeBox(Adis a0, Adis a1)
 {
 	bool af[6];
-	Box b = box[i];
-	for(int i = 0; i < 3; i++)
-	{
-		b.a0.a[i] = (b.edge[i*2] < 0) ? 0 : (box[b.edge[i*2]].a1.a[i]);
-		b.a1.a[i] = (b.edge[i*2+1] < 0) ? EDGE : (box[b.edge[i*2+1]].a0.a[i]);
-	}
-	
-	b.v = (b.a1.a[0] - b.a0.a[0])*(b.a1.a[1] - b.a0.a[1])*(b.a1.a[2] - b.a0.a[2]);
+	Box b = Box(a0, a1);
+	for(int i = 0; i < 6; i++)
+		b.edge[i] = 1;
 	b.t = 0;
+	for(int i = 0; i < nc; i++)
+	{
+		int r = box[i].relation(b);
+		if (r == -2)
+			return b;
+		if(r >= 0)
+			b.edge[r] = 0;
+	}
+	b.t = 1;			//空气
+	b.touch = 0;
 	for(int i = 0; i < 6; i++)
 	{
 		int area;
@@ -127,23 +163,26 @@ void makeBox(int i)
 		else
 			area = (b.a1.a[0] - b.a0.a[0])*(b.a1.a[1] - b.a0.a[1]);
 		
-		if (b.edge[i] >= 0 && b.edge[i] < nc) 
-			b.t += area;
+		if (b.edge[i] == 0)
+			b.touch += area;
 	}
-	box[i] = b;
+	return b;
 }
 
 set<Index> cu;
 void bfs()
 {
-	queue<Index> q;
-	q.push(Index(0,0,0));		//小点
+	queue<pair<Index, int> > q;
+	q.push(make_pair(Index(0,0,0), 0));		//小点
 	
 	nk = 0;
 	area = volume = 0;
 	while(!q.empty())
 	{
-		Index ind = q.front();
+		int edge[6];				//0:铜 1:空气
+		Index ind = (q.front()).first;
+		edge[(q.front()).second] = 1;
+		
 		q.pop();
 		if (cu.count(ind))
 			continue;
@@ -153,10 +192,11 @@ void bfs()
 		for(int i = 0; i < 3; i++)
 		{
 			if(ind.id[i] > 0)
-				q.push(ind.next(10+i));
-			if(ind.id[i] < adis[i].size()-1)
-				q.push(ind.next(i));
-			else
+				q.push(make_pair(ind.next(i*2+1),i*2+1));
+			
+			if(ind.id[i] < adis[i].size()-2)
+				q.push(make_pair(ind.next(i*2), i*2));
+			else if(ind.id[i] > adis[i].size()-2)
 				mb = false;
 		}
 		if (!mb)
@@ -165,10 +205,18 @@ void bfs()
 		Adis a0, a1;
 		for(int i = 0; i < 3; i++)
 		{
-			a0.a[i] = ind.id[i];
-			a1.a[i] = ind.id[i]+1;
+			a0.a[i] = adis[i][ind.id[i]];
+			a1.a[i] = adis[i][ind.id[i]+1];
 		}
-		Box b;		//新空气盒子
+		Box b = makeBox(a0, a1);
+		cout<<b.t<<" "<<b.touch<<" "<<"("<<a0.a[0]<<" "<<a0.a[1]<<" "<<a0.a[2]<<") ("<<a1.a[0]<<" "<<a1.a[1]<<" "<<a1.a[2]<<")"<<endl; 
+		if (b.t == 1)
+		{
+			volume += b.volume;
+			area += b.touch;
+			box[nc+nk] = b;
+			nk++;
+		}
 	}
 	volume = EDGE*EDGE*EDGE - volume;
 }
