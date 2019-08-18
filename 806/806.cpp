125e14f6e29e4fed0df6f7e24495af38831b95e0
ddonix <18959661712@189.cn>
2019-08-18 21:56:52 +0800

    806

---
+++ 806/806.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int manx = 64;
+
+int image[manx][manx];
+int n;
+
+struct Node
+{
+	int v;	//0:black, 1:white, 2:grey
+	int child[4];
+};
+
+int main()
+{
+
+}
